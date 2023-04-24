e8ebaee57f80f4fcca3ec03348fe748c500319c5
Lê Châu Thanh Nhân <61283723+Sealthed@users.noreply.github.com>
2023-04-24 19:43:34 +0700

    Create project.java

---
+++ src/main/java/com/example/project.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "Project")
+public class Project implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "projectid")
+    private Long id;
+
+    @Column(name = "area")
+    private String area;
+
+    @Column(name = "project_name")
+    private String projectName;
+
+    @ManyToOne
+    @JoinColumn(name = "managed_department", referencedColumnName = "Department_id")
+    private Department department;
+}
