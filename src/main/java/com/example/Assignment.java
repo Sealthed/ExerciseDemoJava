41b3fb3ea400dceb8b103751c613217a7db4f302
Lê Châu Thanh Nhân <61283723+Sealthed@users.noreply.github.com>
2023-04-24 19:46:34 +0700

    Create Assignment.java

---
+++ src/main/java/com/example/Assignment.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "Assignment")
+public class Assignment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number_of_hour")
+    private Integer numberOfHour;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", referencedColumnName = "employee_id")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "projectid", referencedColumnName = "projectid")
+    private Project project;
+}
