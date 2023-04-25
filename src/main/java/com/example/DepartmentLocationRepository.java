a00c3bb622bb6102c0dc58d735809fb6bb8eb7a6
Sealthed <rushbeat301@gmail.com>
2023-04-25 09:46:33 +0700

    Bug fixes

--- src/main/java/com/example/DepartmentLocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DepartmentLocationRepository extends CrudRepository<DepartmentLocation, Long> {
-}
-
-
